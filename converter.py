deca80984e118923021e5700dab277d99d05760a
maryam-hamid-sa <maryamhamid.cyber034@gmail.com>
2026-07-09 12:07:29 +0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,15 +1,10 @@
 # Multi Unit Converter 
-
 x = float(input("Enter any number: "))
- 
 print("your answer in meters      :", (x * 1000))
 print("your answer in kilometers  :", (x / 1000))
-
 print("your answer in centimeters :", (x * 2.54))
 print("your answer in inches      :", (x / 2.54))
-
 print("your answer in meters      :", (x * 0.3048))
 print("your answer in feet        :", (x / 0.3048))
-
 print("your answer in kilometers  :", (x * 1.60934))
 print("your answer in miles       :", (x / 1.60934))
